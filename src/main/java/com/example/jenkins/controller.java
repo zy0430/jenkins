e570b88a9f6062533008a18aa7aae2f7e3e24794
root <root@localhost.localdomain>
2019-06-13 22:33:47 +0800

    get 方法返回字符串

---
+++ src/main/java/com/example/jenkins/controller.java
@@ -0,0 +1,17 @@
+package com.example.jenkins;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class controller {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String sayHello(){
+        return "3. Hello jenkins, " + new Date();
+    }
+}
