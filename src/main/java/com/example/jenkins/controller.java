52cacff9ed21e4c944170de6eab9329fec336ba7
z648883413@163.com <z648883413@163.com>
2019-06-17 10:39:08 +0800

    将返回结果修改为 monday77

--- src/main/java/com/example/jenkins/controller.java
+++ src/main/java/com/example/jenkins/controller.java
@@ -12,6 +12,6 @@ public class controller {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String sayHello(){
-        return "monday66 Hello jenkins, " + new Date();
+        return "monday77 Hello jenkins, " + new Date();
     }
 }
